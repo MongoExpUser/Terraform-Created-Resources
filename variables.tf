3982cb16dac6cdfeac111b7e24edad59ef53f3a6
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-04-13 06:00:02 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,46 @@
+#..................................................................................................#
+#                                                                                                  #
+#  @License Starts                                                                                 #
+#                                                                                                  #
+#  Copyright © 2015 - present. MongoExpUser                                                        #
+#                                                                                                  #
+#  License: MIT - https://github.com/MongoExpUser/Terraform-Created-Resources/blob/master/LICENSE  #
+#                                                                                                  #
+#  @License Ends                                                                                   #
+#                                                                                                  #
+#..................................................................................................#
+#  variables.tf                                                                                    #
+#..................................................................................................#
+
+
+#define non-provider variable(s)
+variable "number_of_instance" {
+  default = 2
+}
+
+variable "provider_name" {
+ default = "AWS"
+}
+
+variable "region" {
+ default = "us-east-1"
+}
+
+variable "ec2_instance_type" {
+  #default = "t2.micro"
+  default = "t3a.nano"
+}
+
+variable "filter_name_values" {
+ default = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+}
+
+variable "filter_virtualization_type_values" {
+ default = ["hvm"]
+}
+
+variable "owners" {
+ default = ["099720109477"] # Canonical
+}
+
+# add more variables as necessary or desired.
