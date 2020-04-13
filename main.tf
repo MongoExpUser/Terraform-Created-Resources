1c867ac7a0322434f6ea2ea967cd7263c9867be5
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-04-13 06:00:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,40 +12,7 @@
 #  main.tf                                                                                         #
 #..................................................................................................#
 
-#define non-provider variable(s)
-variable "number_of_instance" {
-  default = 2
-}
-
-variable "provider_name" {
- default = "AWS"
-}
-
-variable "region" {
- default = "us-east-1"
-}
-
-variable "ec2_instance_type" {
-  #default = "t2.micro"
-  default = "t3a.nano"
-}
-
-variable "filter_name_values" {
- default = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-}
-
-variable "filter_virtualization_type_values" {
- default = ["hvm"]
-}
-
-variable "owners" {
- default = ["099720109477"] # Canonical
-}
-
-# add more variables as necessary or desired.
-
-
-# aws ec2 instance(s)
+# # create aws ec2 instance(s)
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -59,7 +26,7 @@ data "aws_ami" "ubuntu" {
     values = "${var.filter_virtualization_type_values}"
   }
   
-  owners = "${var.owners}"
+  owners   = "${var.owners}"
 }
 
 
