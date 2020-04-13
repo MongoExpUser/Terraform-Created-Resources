012372d6984fa157a3cde24d7ba55317afff12e2
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-04-13 04:42:20 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -26,7 +26,19 @@ variable "region" {
 
 variable "ec2_instance_type" {
   #default = "t2.micro"
-  default = "t3a.nano" 
+  default = "t3a.nano"
 }
 
-# add more variables as necessary or desired
+variable "filter_name_values" {
+ default = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+}
+
+variable "filter_virtualization_type_values" {
+ default = ["hvm"]
+}
+
+variable "owners" {
+ default = ["099720109477"] # Canonical
+}
+
+# add more variables as necessary or desired.
