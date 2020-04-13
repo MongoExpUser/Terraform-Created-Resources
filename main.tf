a6b3588cdf2af89db2f2ba78128f953b4d9fafe5
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-04-13 03:25:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,41 @@
-#
+#..................................................................................................#
+#                                                                                                  #
+#  @License Starts                                                                                 #
+#                                                                                                  #
+#  Copyright © 2015 - present. MongoExpUser                                                        #
+#                                                                                                  #
+#  License: MIT - https://github.com/MongoExpUser/Terraform-Created-Resources/blob/master/LICENSE  #
+#                                                                                                  #
+#  @License Ends                                                                                   #
+#                                                                                                  #
+#..................................................................................................#
+#  main.tf                                                                                         #
+#..................................................................................................#
+
+# aws ec2 instance(s)
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+  
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+resource "aws_instance" "aws_ec2_web_server" {
+  count           = "${var.number_of_instance}"
+  ami             = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "${var.ec2_instance_type}"
+
+  tags = {
+    Name = "${var.region}-${count[index]}-ubuntu-bionic}"
+  }
+}
+
+# add more resourcess as necessary or desired.
