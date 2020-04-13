b4a4967bdb7fca6b591ca3d5364a350c00a28b8b
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-04-13 06:01:38 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,9 +12,8 @@
 #  outputs.tf                                                                                      #
 #..................................................................................................#
 
+#define output(s)
 output "aws_ec2_instances" {
   description = "A list of all created AWS' EC2 instances"
   value = "${aws_instance.aws_ec2_web_server}"
 }
-
-# add more outputs as necessary or desired.
