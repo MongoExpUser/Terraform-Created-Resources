272573be5a4a0ee4cd309a0fb9c209bd206a9f3f
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-04-13 04:43:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,19 +12,18 @@
 #  main.tf                                                                                         #
 #..................................................................................................#
 
-
 # aws ec2 instance(s)
 data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = "${var.filter_name_values}"
   }
   
   filter {
     name   = "virtualization-type"
-    values = ["hvm"]
+    values = "${var.filter_virtualization_type_values}"
   }
   
   owners = "${var.owners}"
@@ -36,7 +35,7 @@ resource "aws_instance" "aws_ec2_web_server" {
   instance_type   = "${var.ec2_instance_type}"
 
   tags = {
-    Name = "${var.region}-${count[index]}-ubuntu-bionic}"
+    Name = "${var.region}-ubuntu-bionic}"
   }
 }
 
