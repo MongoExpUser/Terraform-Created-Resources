3872f5a6d7a17a495b039b6db5e20fc5af2d0aac
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-04-15 16:55:55 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,8 +13,8 @@
 #..................................................................................................#
 
 
-#define non-provider variable(s)
-variable "number_of_instance" {
+#define non-provider variable(s) for ec2
+variable "ec2_instance_number" {
   default = 2
 }
 
@@ -43,4 +43,26 @@ variable "owners" {
  default = ["099720109477"] # Canonical
 }
 
-# add more variables as necessary or desired.
+
+#define non-provider variable(s) for lightsail
+variable "lightsail_instance_names" {
+  description = "A list of lightsail names of type string"
+  default = ["lightsail_mysql_server", "lightsail_mongodb_server"]
+}
+
+variable "light_sail_availability_zone" {
+  default = "us-east-1a"
+}
+
+variable "lightsail_blueprint_id" {
+  default = "ubuntu_18_04"
+}
+
+variable "lightsail_bundle_id" {
+  default = "nano_2_0"
+}
+
+variable "lightsail_tags_values" {
+  description = "A list of lightsail tags' values of type string"
+  default = ["db1_mysql", "db2_mongodb"]
+}
