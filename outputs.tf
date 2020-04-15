965040b28aadadf690982b49ce092d5bcb3ca2f3
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-04-15 16:42:31 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,8 +12,16 @@
 #  outputs.tf                                                                                      #
 #..................................................................................................#
 
+
 #define output(s)
 output "aws_ec2_instances" {
   description = "A list of all created AWS' EC2 instances"
   value = "${aws_instance.aws_ec2_web_server}"
 }
+
+output "aws_lightsail_instances" {
+  description = "A list of all created AWS' lightsail instances"
+  value = "${aws_lightsail_instance.aws_lightsail_db_server}"
+}
+
+# add more outputs as necessary or desired.
