b5035b8e6c8be88be7ae537ff232cbd003cf1ae1
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-04-18 11:39:52 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,19 +25,19 @@ variable "region" {
 }
 
 variable "ec2_web_server_ami_numbers" {
-  #note: Ubuntu Server 18.04 LTS (HVM), SSD Volume Type => ami-07ebfd5b3428b6f4d (64-bit x86)
-  #note: Amazon Linux 2 LTS with .NET Core 3.1, Mono 5.18 and PowerShell 6.2 pre-installed => ami-01fbc92d24852df9b
-  #note: both have-> (1) Root device type: ebs, (2) Virtualization type: hvm, and  (3) ENA Enabled: Yes
+  # note: Ubuntu Server 18.04 LTS (HVM), SSD Volume Type => ami-07ebfd5b3428b6f4d (64-bit x86)
+  # note: Amazon Linux 2 LTS with .NET Core 3.1, Mono 5.18 and PowerShell 6.2 pre-installed => ami-01fbc92d24852df9b
+  # note: both have-> (1) Root device type: ebs, (2) Virtualization type: hvm, and  (3) ENA Enabled: Yes
   default = ["ami-07ebfd5b3428b6f4d", "ami-01fbc92d24852df9b"]
 }
 
 variable "ec2_web_server_types" {
-  #length must equal length of "ec2_db_server_ami_number"
-  #note: cpu_core_count is not supported on some instances including "t3a.nano"
-  #note: for instances that support cpu_core_count, see url below:
+  # length must equal length of "ec2_db_server_ami_number"
+  # note: cpu_core_count is not supported on some instances including "t3a.nano"
+  # note: for instances that support cpu_core_count, see url below:
   #    : https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-optimize-cpu.html#cpu-options-supported-instances-values
   default = ["t3a.nano", "t3a.nano"]
-  #default = ["g3s.xlarge", "g3s.xlarge"]
+  # default = ["g3s.xlarge", "g3s.xlarge"]
 }
 
 variable "ec2_web_server_cpu_core_count" {
@@ -59,15 +59,15 @@ variable "ec_web_server_tags_values" {
 
 # A2. database servers
 variable "ec2_db_server_ami_numbers" {
-  #note: Ubuntu Server 18.04 LTS (HVM), SSD Volume Type => ami-07ebfd5b3428b6f4d (64-bit x86)
-  #note: (1) Root device type: ebs, (2) Virtualization type: hvm, and  (3) ENA Enabled: Yes
+  # note: Ubuntu Server 18.04 LTS (HVM), SSD Volume Type => ami-07ebfd5b3428b6f4d (64-bit x86)
+  # note: (1) Root device type: ebs, (2) Virtualization type: hvm, and  (3) ENA Enabled: Yes
   default = ["ami-07ebfd5b3428b6f4d", "ami-07ebfd5b3428b6f4d"]
 }
 
 variable "ec2_db_server_types" {
-   #length must equal length of "ec2_db_server_ami_number"
-  #note: cpu_core_count is not supported on some instances including "t3a.nano"
-  #note: for instances that support cpu_core_count, see url below:
+  # length must equal length of "ec2_db_server_ami_number"
+  # note: cpu_core_count is not supported on some instances including "t3a.nano"
+  # note: for instances that support cpu_core_count, see url below:
   #    : https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-optimize-cpu.html#cpu-options-supported-instances-values
   default = ["t3a.nano", "t3a.nano"]
   #default = ["g3s.xlarge", "g3s.xlarge"]
