d88e393afabf7395e2d883e8af9a96ea4cb5c819
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-04-18 12:02:43 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,3 @@
-#..................................................................................................#
-#                                                                                                  #
-#  @License Starts                                                                                 #
-#                                                                                                  #
-#  Copyright © 2015 - present. MongoExpUser                                                        #
-#                                                                                                  #
-#  License: MIT - https://github.com/MongoExpUser/Terraform-Created-Resources/blob/master/LICENSE  #
-#                                                                                                  #
-#  @License Ends                                                                                   #
-#                                                                                                  #
-#..................................................................................................#
-#  outputs.tf                                                                                      #
-#..................................................................................................#
-
-
 #define output(s)
 output "aws_ec2_web_servers" {
   description = "A list of all created EC2 web server instances"
@@ -26,7 +11,7 @@ output "aws_ec2_db_servers" {
   value = aws_instance.aws_ec2_db_server
 }
 
-output "aws_lightsail_instances_database_servers" {
+output "aws_lightsail_db_servers" {
   description = "A list of all created AWS lightsail db server instances"
   # the list contains key-value pairs of each instance's attributes
   value = aws_lightsail_instance.aws_lightsail_db_server
