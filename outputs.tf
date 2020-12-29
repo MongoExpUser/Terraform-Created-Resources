0c8be3380c8f9c533e4b8df93853a8f3612e286f
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-12-28 20:23:34 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,18 @@
+#..................................................................................................#
+#                                                                                                  #
+#  @License Starts                                                                                 #
+#                                                                                                  #
+#  Copyright © 2015 - present. MongoExpUser                                                        #
+#                                                                                                  #
+#  License: MIT - https://github.com/MongoExpUser/Terraform-Created-Resources/blob/master/LICENSE  #
+#                                                                                                  #
+#  @License Ends                                                                                   #
+#                                                                                                  #
+#..................................................................................................#
+#  outputs.tf                                                                                      #
+#..................................................................................................#
+
+
 #define output(s)
 output "aws_ec2_web_servers" {
   description = "A list of all created EC2 web server instances"
@@ -15,6 +30,13 @@ output "aws_lightsail_servers" {
   description = "A list of all created AWS lightsail server instances"
   # the list contains key-value pairs of each instance's attributes
   value = aws_lightsail_instance.aws_lightsail_server
+}
+
+
+output "aws_lightsail_static_ips" {
+  description = "A list of all created AWS lightsail static ips"
+  # the list contains key-value pairs of each instance's attributes
+  value = aws_lightsail_static_ip.aws_lightsail_static_ips
 }
 
 # add more outputs as necessary or desired.
