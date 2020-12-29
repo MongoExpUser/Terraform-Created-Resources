9a9a64fefb2dbc971b708b6bb30ae35ecad5cb71
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2020-12-29 03:24:47 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@
 #  main.tf                                                                                         #
 #..................................................................................................#
 
+
 # create aws ec2 instance(s), using "aws_instance" statement with ami-number
 resource "aws_instance" "aws_ec2_web_server" {
   count                 = length(var.ec2_web_server_ami_numbers)
@@ -23,7 +24,7 @@ resource "aws_instance" "aws_ec2_web_server" {
   #cpu_threads_per_core = var.ec2_web_server_cpu_threads_per_core
   
   # user data file: bash shell script (start-up script)
-  user_data             = file(var.user_data_file_path)
+  user_data             = file(var.web_server_user_data_file_path)
   tags = {
     ec2_key = var.ec_web_server_tags_values[count.index]
   }
@@ -38,41 +39,68 @@ resource "aws_instance" "aws_ec2_db_server" {
   #cpu_core_count       = var.ec2_db_server_cpu_core_count
   #cpu_threads_per_core = var.ec2_db_server_cpu_threads_per_core
   
-  # user data file: bash shell script (start-up script)
-  user_data             = file(var.user_data_file_path)
   tags = {
-    ec2_key = var.ec_db_server_tags_values[count.index]
+    key = var.ec_db_server_tags_values[count.index]
   }
 }
+
 
 # create lightsail instance(s)
-resource "aws_lightsail_instance" "aws_lightsail_server" {
-  count                 = length(var.lightsail_names)
-  name                  = var.lightsail_names[count.index]
-  availability_zone     = var.lightsail_availability_zone
-  blueprint_id          = var.lightsail_blueprint_ids[count.index]
-  bundle_id             = var.lightsail_bundle_ids[count.index]
+resource "aws_lightsail_instance" "aws_lightsail_web_server" {
+  count                 = length(var.lightsail_web_server_names)
+  name                  = var.lightsail_web_server_names[count.index]
+  availability_zone     = var.lightsail_web_server_availability_zone
+  blueprint_id          = var.lightsail_web_server_blueprint_ids[count.index]
+  bundle_id             = var.lightsail_web_server_bundle_ids[count.index]
   # user data file: bash shell script (start-up script)
-  user_data             = file(var.user_data_file_path)
+  user_data             = file(var.web_server_user_data_file_path)
   tags = {
-    key = var.lightsail_tags_values[count.index]
+    key = var.lightsail_web_server_tags_values[count.index]
   }
 }
 
-# create lightsail instance(s) static ip(s)
-resource "aws_lightsail_static_ip" "aws_lightsail_static_ips" {
-  count      = length(var.aws_lightsail_static_ip)
-  name       = var.aws_lightsail_static_ip[count.index]
-  depends_on = [aws_lightsail_instance.aws_lightsail_server]
+resource "aws_lightsail_instance" "aws_lightsail_db_server" {
+  count                 = length(var.lightsail_db_server_names)
+  name                  = var.lightsail_db_server_names[count.index]
+  availability_zone     = var.lightsail_db_server_availability_zone
+  blueprint_id          = var.lightsail_db_server_blueprint_ids[count.index]
+  bundle_id             = var.lightsail_db_server_bundle_ids[count.index]
+  tags = {
+    key = var.lightsail_db_server_tags_values[count.index]
+  }
 }
 
 
-# attach lightsail instance(s) to static ip(s)
-resource "aws_lightsail_static_ip_attachment" "aws_lightsail_static_ip_attachments" {
-  count          = length(var.lightsail_names)
-  static_ip_name = aws_lightsail_static_ip.aws_lightsail_static_ips[count.index].id
-  instance_name  = aws_lightsail_instance.aws_lightsail_server[count.index].id
-  depends_on     = [aws_lightsail_static_ip.aws_lightsail_static_ips]
+# create lightsail instance(s) static ip(s): web server
+resource "aws_lightsail_static_ip" "aws_lightsail_web_server_static_ips" {
+  count         = length(var.lightsail_web_server_static_ip)
+  name          = var.lightsail_web_server_static_ip[count.index]
+  depends_on    = [aws_lightsail_instance.aws_lightsail_web_server]
 }
+
+# attach lightsail instance(s) to static ip(s): web server
+resource "aws_lightsail_static_ip_attachment" "aws_lightsail_web_server_static_ip_attachments" {
+  count          = length(var.lightsail_web_server_names)
+  static_ip_name = aws_lightsail_static_ip.aws_lightsail_web_server_static_ips[count.index].id
+  instance_name  = aws_lightsail_instance.aws_lightsail_web_server[count.index].id
+  depends_on     = [aws_lightsail_static_ip.aws_lightsail_web_server_static_ips]
+}
+
+
+# create lightsail instance(s) static ip(s): db server
+resource "aws_lightsail_static_ip" "aws_lightsail_db_server_static_ips" {
+  count         = length(var.lightsail_db_server_static_ip)
+  name          = var.lightsail_db_server_static_ip[count.index]
+  depends_on    = [aws_lightsail_instance.aws_lightsail_db_server]
+}
+
+# attach lightsail instance(s) to static ip(s): db server
+resource "aws_lightsail_static_ip_attachment" "aws_lightsail_db_server_static_ip_attachments" {
+  count          = length(var.lightsail_db_server_names)
+  static_ip_name = aws_lightsail_static_ip.aws_lightsail_db_server_static_ips[count.index].id
+  instance_name  = aws_lightsail_instance.aws_lightsail_db_server[count.index].id
+  depends_on     = [aws_lightsail_static_ip.aws_lightsail_db_server_static_ips]
+}
+
 
 # add more resourcess as necessary or desired.
